bb55a601484e0bbe6298cb91261692e2bce11ead
gentian <genti_mitro@outlook.com>
2023-05-21 21:31:53 +0200

    List files

--- Utilis/directory.py
+++ Utilis/directory.py
@@ -5,3 +5,9 @@ def make_directory(path):
     directory_exists = os.path.exists(path)
     if not directory_exists:
         os.makedirs(path)
+
+
+def list_files(path):
+    files = os.listdir(path)
+    files = [f for f in files if os.path.isfile(path+'/'+f)]
+    print(*files, sep="\n")
